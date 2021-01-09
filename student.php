6e1b1d33922b2c5974f8bb7e66e9b324ff29415b
Tech4All <tech4all583@gmail.com>
2021-01-09 21:43:10 +0100

    inital

--- student.php
+++ student.php
@@ -11,4 +11,43 @@ require_once 'config/core.php';
 require_once 'libs/head.php';
 ?>
 
+<section class="content">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><?= $page_title ?></h3>
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                                title="Collapse">
+                            <i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="box-body">
+
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped table-hover" id="example1">
+                            <thead>
+                            <tr>
+                                <th>SN</th>
+                                <th>Application Id</th>
+                                <th>Full Name</th>
+                                <th>Class</th>
+                                <th>Terms</th>
+                                <th>Parent Name</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
 <?php require_once 'libs/foot.php'?>
