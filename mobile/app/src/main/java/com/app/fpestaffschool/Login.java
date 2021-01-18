0bb4615122ef9cf50491f2282dfcba6b9df6fa11
Tech4All <tech4all583@gmail.com>
2021-01-18 17:02:30 +0100

    initial

--- mobile/app/src/main/java/com/app/fpestaffschool/Login.java
+++ mobile/app/src/main/java/com/app/fpestaffschool/Login.java
@@ -2,7 +2,10 @@ package com.app.fpestaffschool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.setTitle("Parent Login");
+
+
     }
+
 }
